82441dbf0eae5bcf08bc2cc653431cd41e8a75c8
Josefin <josefin.oleryd@gmail.com>
2025-12-27 15:01:36 +0100

    added markdown for vis2 (method)

--- data/vis2.sql
+++ data/vis2.sql
@@ -26,24 +26,6 @@ INNER JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.Pr
 INNER JOIN Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID
 WHERE pc.Name = 'Bikes' 
 
-
--- Men vilken valuta är det som gäller? 
--- Står inte i SalesOrderDetail
--- Utan finns "högre upp" -> SalesOrderHeader
-SELECT * FROM Sales.SalesOrderHeader
-
--- Hittade kolum CurrencyRateID
--- Många NULL, men hur många?
-SELECT
-    COUNT(*) AS Totalt,
-    SUM(CASE WHEN CurrencyRateID IS NULL THEN 1 ELSE 0 END) AS AntalNULL,
-    ROUND(
-        100.0 * SUM(CASE WHEN CurrencyRateID IS NULL THEN 1 ELSE 0 END) / COUNT(*),
-        2
-    ) AS AndelNULLProcent
-FROM Sales.SalesOrderHeader
--- NULL = ingen valutaväxling har skett
-
 -- Vilka tabller finns det som heter något med Currency?
 SELECT
     TABLE_SCHEMA,
@@ -51,18 +33,12 @@ SELECT
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_NAME LIKE '%Currency%'
 
+-- Resultat av ovan
+SELECT * FROM Sales.CountryRegionCurrency
+SELECT * FROM Sales.Currency
 SELECT * FROM Sales.CurrencyRate
 
--- Vad händer på de rader där CurrencyRateID inte är NULL?
-SELECT
-    cr.FromCurrencyCode,
-    cr.ToCurrencyCode,
-    COUNT (*) AS AntalOrdrar
-FROM Sales.SalesOrderHeader soh
-JOIN Sales.CurrencyRate cr ON soh.CurrencyRateID = cr.CurrencyRateID
-GROUP BY cr.FromCurrencyCode, cr.ToCurrencyCode
-ORDER BY AntalOrdrar DESC
--- Valutan går från USD till kundens valuta, alltså är intäkterna i SalesOrderDetail USD
-
-
+-- SalesCurrencyRate har kolumnen FromCurrencyCode 
+SELECT DISTINCT FromCurrencyCode
+FROM Sales.CurrencyRate
 
