d7808ea35513d42ca6914520f2ad23c358bbb92f
Josefin <josefin.oleryd@gmail.com>
2026-01-02 14:51:17 +0100

    added code and markdown for first part of in-depth analysis

---
+++ data/alt_c_question1.sql
@@ -0,0 +1,39 @@
+SELECT * FROM Sales.SalesOrderHeader
+SELECT * FROM Sales.Customer
+SELECT * FROM Sales.Store
+SELECT * FROM Person.BusinessEntityContact
+
+SELECT
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME LIKE '%PersonID%'
+
+
+SELECT 
+    CustomerID,
+    MAX(OrderDate) AS LastOrderDate,
+    CASE
+        WHEN MAX(OrderDate) < DATEADD(MONTH, -12, '2025-06-01')
+        THEN 'Churned'
+        ELSE 'Active'
+    END AS CustomerStatus
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID
+ORDER BY LastOrderDate ASC
+
+
+SELECT
+    ChurnYear,
+    COUNT(*) AS NumberOfChurnedCustomers
+FROM (
+    SELECT
+        CustomerID,
+        YEAR(MAX(OrderDate)) AS ChurnYear
+    FROM Sales.SalesOrderHeader
+    GROUP BY CustomerID
+    HAVING MAX(OrderDate) < '2024-06-01'
+) AS CustomerChurn
+GROUP BY ChurnYear
+ORDER BY ChurnYear
